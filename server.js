ef741047a4d42acd645b575eeff5fc501dfe151c
ellie <ellie.tms1371@gmail.com>
2020-07-04 19:54:35 +0100

    add new codes

--- server.js
+++ server.js
@@ -1,23 +1,83 @@
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
+const bodyParser = require("body-parser");
+const data = require("./data.json");
 
-app.use(cors());
-
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-};
-
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+// what you need to expect to understand client content
+// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.raw());
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+// Read all messages
+app.get("/messages", function (req, res) {
+  res.json(data);
+});
+
+// level 3
+app.post("/messages/search", (req, res) => {
+  const text = `${req.query.text.toLowerCase()}`;
+  const messageWithSpecificText = data.filter((message) =>
+    message.text.toLowerCase().includes(text)
+  );
+  res.json(messageWithSpecificText);
+});
+
+// level 3
+app.get("/messages/latest", (req, res) => {
+  if (data.length >= 10) {
+    const latestMessages = data.slice(data.length - 10, data.length + 1);
+    res.json(latestMessages);
+  } else if (data.length < 10) {
+    res.json(data);
+  } else {
+    res.json("Sorry,you do not have enough message");
+  }
+});
+
+// Create a new message
+app.post("/messages/newMessage", (req, res) => {
+  // level 2
+  if (req.body.from === "" || req.body.text === "") {
+    res
+      .status(400)
+      .json("Bad request,Please make sure all fields are filled in correctly");
+  } else {
+    // Here
+    //question:
+    //  if I want to have new id after those information I added to my data, how can I arrange my next id to be after this id I added(sorted ones)
+    const randomId = Math.floor(Math.random() * (2 * data.length) + 6);
+    data.push({
+      id: randomId,
+      from: req.body.from,
+      text: req.body.text,
+      timeSent: new Date(),
+    });
+    res.json(data);
+  }
+});
+
+// read one message specified by an Id
+app.get("/messages/:id", (req, res) => {
+  const { id } = req.params;
+  const messageWithSpecificId = data.filter((message) => message.id === id);
+  res.json(messageWithSpecificId);
+});
+
+// Delete a message by Id
+app.delete("/messages/:id", (req, res) => {
+  const { id } = req.params;
+  const messageWithSpecificId = data.filter((message) => message.id !== id);
+  res.json(messageWithSpecificId);
+});
+
+// level 5
+// add level 5 (put request)
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
+});
