752b22e7f68e593c9fa1a355d89c23092e5f0177
ellie <ellie.tms1371@gmail.com>
2020-07-09 22:16:26 +0100

    change const to let

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const data = require("./data.json");
+let data = require("./data.json");
 const cors = require("cors");
 
 // what you need to expect to understand client content
